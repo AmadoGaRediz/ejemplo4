8445c7259db89baea76dc9ee7d959ba653b6a6c0
AmadoGaRediz <135990516+AmadoGaRediz@users.noreply.github.com>
2025-02-03 12:17:52 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],  
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ejemplo4';
+
+  //propiedades
+  nombreCliente: string = '';
+  emailCliente: string = '';
+  opinionCliente: string = '';
+  statusCliente: string = '';
+  municipioCliente: string = '';
+  autorizacionCliente: boolean = false;
+
+  enviado = false;
+
+
+  //declaramos el formulario
+  formOpinion = new FormGroup({
+    nombre: new FormControl('',[Validators.required, Validators.minLength(10)]),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    opinion: new FormControl('',[Validators.required, Validators.maxLength(500)]),
+    status: new FormControl('',[Validators.required]),
+    municipio: new FormControl('',[Validators.required]),
+    autorizacion: new FormControl(false)
+  });
+
+  //metodo que se ejecuta cuando se envia el formulario
+  onSubmit() {
+    this.enviado = true;
+    if (this.formOpinion.valid) {
+      this.nombreCliente = this.formOpinion.value.nombre!;
+      this.emailCliente = this.formOpinion.value.email!;
+      this.opinionCliente = this.formOpinion.value.opinion!;
+      this.statusCliente = this.formOpinion.value.status!;              
+      this.municipioCliente = this.formOpinion.value.municipio!;
+      this.autorizacionCliente = this.formOpinion.value.autorizacion!;
+    }
+  }
+
+  
+
+
 }
